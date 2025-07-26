9aef0095b0b2a43d1a8fcc86d916b76a18d72fcb
Vahid Hasanpour <dr.vet8369@gmail.com>
2025-07-26 23:06:35 +0000

    خب خوبه فقط اون دکمه ضربدر دورش یه لایه سبزرنگ داره اونو بکن سفیدنظرت چی

--- src/app/provider/[providerId]/page.tsx
+++ src/app/provider/[providerId]/page.tsx
@@ -288,7 +288,7 @@ export default function ProviderProfilePage() {
                                 <DialogHeader className="sr-only">
                                   <DialogTitle>نمونه کار تمام صفحه</DialogTitle>
                                 </DialogHeader>
-                               <DialogClose className="absolute right-4 top-4 rounded-full p-2 bg-black/50 text-white opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none z-50">
+                               <DialogClose className="absolute right-4 top-4 rounded-full p-2 bg-black/50 text-white opacity-70 transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-black disabled:pointer-events-none z-50">
                                   <X className="h-6 w-6" />
                                   <span className="sr-only">بستن</span>
                                 </DialogClose>
