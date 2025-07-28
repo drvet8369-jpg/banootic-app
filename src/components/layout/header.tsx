fc777e2819b567531879bc2e2019d6dd81aa71a9
Vahid Hasanpour <dr.vet8369@gmail.com>
2025-07-28 08:58:24 +0000

    نه نمیشه.تواون یه خط همبرگری و لوگوی بالای سمت راست روکش سمت گوشه ها وجا

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -211,7 +211,7 @@ export default function Header() {
   return (
     <>
       <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-        <div className="container flex h-16 items-center justify-between gap-4 mx-auto">
+        <div className="container flex h-16 items-center gap-4 mx-auto">
           <Link href="/" className="flex items-center gap-2">
             <Logo className="h-10 w-10 text-primary-foreground" />
             <span className="font-display text-2xl font-bold whitespace-nowrap">هنربانو</span>
