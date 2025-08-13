662c2b28223a25b74e04bedc769a7265a4dc807a
Vahid Hasanpour <dr.vet8369@gmail.com>
2025-08-13 22:21:02 +0000

    منظورم سایز وشکل خودخطوط همبرگری هست نه عملکردشون.من خطوط همبرگری  اپ رو

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -174,7 +174,11 @@ export default function Header() {
                 <Sheet open={isSheetOpen} onOpenChange={setIsSheetOpen}>
                 <SheetTrigger asChild>
                     <Button variant="ghost" size="icon">
-                    <Menu className="h-6 w-6" />
+                     <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" className="h-6 w-6">
+                        <path d="M4 6H20" stroke="currentColor" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round"/>
+                        <path d="M4 12H20" stroke="currentColor" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round"/>
+                        <path d="M4 18H20" stroke="currentColor" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round"/>
+                    </svg>
                     <span className="sr-only">باز کردن منو</span>
                     </Button>
                 </SheetTrigger>
