c97af3d3ef3ccc531d29e6027d6271541191e97d
Vahid Hasanpour <dr.vet8369@gmail.com>
2025-08-17 08:21:37 +0000

    بجای پررنگ شدن برعکس کمرنگترشد

--- src/components/ui/search-bar.tsx
+++ src/components/ui/search-bar.tsx
@@ -23,7 +23,7 @@ export default function SearchBar() {
           <Input
             type="search"
             placeholder="جستجو در میان هنرمندان و خدمات..."
-            className="w-full pr-10 bg-background placeholder:font-semibold text-foreground placeholder:text-foreground/60 border-2 border-primary/40 focus-visible:ring-primary/80 focus-visible:ring-offset-2"
+            className="w-full pr-10 bg-background placeholder:font-semibold text-foreground placeholder:text-foreground/60 border-2 border-primary focus-visible:ring-primary/80 focus-visible:ring-offset-2"
             value={searchTerm}
             onChange={(e) => setSearchTerm(e.target.value)}
           />
