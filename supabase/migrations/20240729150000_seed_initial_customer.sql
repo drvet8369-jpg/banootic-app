346ec3053303d91380881964d440be9ebb4d815a
Vahid Hasanpour <dr.vet8369@gmail.com>
2025-08-18 09:30:07 +0000

    ما بیشتراز یک مشتری در اپ نداریم وآن هم مژگان جودکی است .خب اگر در دیتاب

---
+++ supabase/migrations/20240729150000_seed_initial_customer.sql
@@ -0,0 +1,6 @@
+
+-- Seed an initial customer for testing purposes
+
+INSERT INTO public.customers (name, phone, account_type)
+VALUES ('مژگان جودکی', '09121111111', 'customer')
+ON CONFLICT (phone) DO NOTHING;
