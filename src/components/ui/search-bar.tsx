8ca133cf86fef86f89f4b43e675dd60135ef01bd
Vahid Hasanpour <dr.vet8369@gmail.com>
2025-08-19 11:05:57 +0000

    متن داخل نوارجستجورا سیاه کن طوری که با رنگ خط دورنوار یکرنگ باشد

--- src/components/ui/search-bar.tsx
+++ src/components/ui/search-bar.tsx
@@ -23,7 +23,7 @@ export default function SearchBar() {
           <Input
             type="search"
             placeholder="جستجو در میان هنرمندان و خدمات..."
-            className="w-full pr-10 bg-background placeholder:font-semibold text-black border-2 border-black focus-visible:ring-primary/80 focus-visible:ring-offset-2"
+            className="w-full pr-10 bg-background placeholder:font-semibold placeholder:text-black text-black border-2 border-black focus-visible:ring-primary/80 focus-visible:ring-offset-2"
             value={searchTerm}
             onChange={(e) => setSearchTerm(e.target.value)}
           />
