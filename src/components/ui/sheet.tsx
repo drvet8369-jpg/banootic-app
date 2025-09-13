219f1178631becd015cc829eeb6db4f5652ca6ee
Vahid Hasanpour <dr.vet8369@gmail.com>
2025-09-13 21:36:08 +0000

    آیکون ضربدر که برای بستن صفحه کشویی منو استفاده میشه خیلی کمرنگه ومربع د

--- src/components/ui/sheet.tsx
+++ src/components/ui/sheet.tsx
@@ -64,9 +64,8 @@ const SheetContent = React.forwardRef<
       className={cn(sheetVariants({ side }), className)}
       {...props}
     >
-      <SheetPrimitive.Title className="sr-only">Menu</SheetPrimitive.Title>
       {children}
-      <SheetPrimitive.Close className="absolute left-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:text-muted-foreground">
+      <SheetPrimitive.Close className="absolute left-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none">
         <X className="h-4 w-4" />
         <span className="sr-only">Close</span>
       </SheetPrimitive.Close>
