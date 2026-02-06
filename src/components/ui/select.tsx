59760d55a8468efb3765b1c62de13dbdb35af187
Vahid Hasanpour <dr.vet8369@gmail.com>
2026-02-06 09:15:33 +0000

    خب حالا آن ۴ دسته بندی کشویی نیز درسمت چپ قرلردارند وهم چنین آن گزینه ها

--- src/components/ui/select.tsx
+++ src/components/ui/select.tsx
@@ -20,7 +20,7 @@ const SelectTrigger = React.forwardRef<
     ref={ref}
     className={cn(
       "flex h-10 w-full items-center justify-between rounded-md border-2 border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus:outline-none disabled:cursor-not-allowed disabled:opacity-50 [&>span]:line-clamp-1",
-      "flex-row-reverse",
+      "flex-row-reverse text-right",
       className
     )}
     {...props}
@@ -106,7 +106,7 @@ const SelectLabel = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <SelectPrimitive.Label
     ref={ref}
-    className={cn("py-1.5 pr-8 pl-2 text-sm font-semibold", className)}
+    className={cn("py-1.5 pr-8 pl-2 text-sm font-semibold text-right", className)}
     {...props}
   />
 ))
@@ -120,6 +120,7 @@ const SelectItem = React.forwardRef<
     ref={ref}
     className={cn(
       "relative flex w-full cursor-default select-none items-center rounded-sm py-1.5 pr-8 pl-2 text-sm outline-none focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50",
+      "text-right",
       className
     )}
     {...props}
